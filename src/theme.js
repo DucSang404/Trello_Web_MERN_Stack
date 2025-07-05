c3b5c9916c78ba432f07300b49da45c3b78a93c8
DucSang404 <ndsang404@gmail.com>
2025-07-05 21:18:21 +0700

    refactor directory tree

--- src/theme.js
+++ src/theme.js
@@ -1,30 +1,30 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
-import { cyan, deepOrange, deepPurple, green, lightBlue, orange, red, teal } from '@mui/material/colors'
+import { deepOrange, deepPurple, green, orange } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = extendTheme({
-    trello: {
-        appBarHeight: '48px',
-        boardBarHeight: '58px',
+  trello: {
+    appBarHeight: '48px',
+    boardBarHeight: '58px'
+  },
+
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: green,
+        secondary: deepOrange
+      },
+      spacing: (factor) => `${0.25 * factor}rem`
     },
-
-    colorSchemes: {
-        light: {
-            palette: {
-                primary: green,
-                secondary: deepOrange
-            },
-            spacing: (factor) => `${0.25 * factor}rem`
-        },
-        dark: {
-            palette: {
-                primary: deepPurple,
-                secondary: orange
-            },
-            spacing: (factor) => `${0.25 * factor}rem`
-        }
+    dark: {
+      palette: {
+        primary: deepPurple,
+        secondary: orange
+      },
+      spacing: (factor) => `${0.25 * factor}rem`
     }
-});
+  }
+})
 
 
-export default theme;
+export default theme
