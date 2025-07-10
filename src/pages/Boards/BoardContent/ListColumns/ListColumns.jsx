c57f02689de69797a1da413115481e93cc4ba4e9
DucSang404 <ndsang404@gmail.com>
2025-07-10 21:04:22 +0700

    listcolumnjsx and mockdata

--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -2,6 +2,9 @@ import {
   Box
 } from '@mui/material'
 import Column from './Column/Column'
+import Button from '@mui/material/Button'
+import NoteAddIcon from '@mui/icons-material/NoteAdd'
+
 function ListColumns() {
 
   return (
@@ -17,6 +20,29 @@ function ListColumns() {
     }}>
       <Column />
       <Column />
+      <Column />
+
+      <Box sx={{
+        minWidth: '200px',
+        maxWidth: '200px',
+        mx: 2,
+        borderRadius: '6px',
+        height: 'fit-content',
+        bgcolor: '#ffffff3d'
+      }}>
+        <Button
+          startIcon={<NoteAddIcon />}
+          sx={{
+            color: 'white',
+            width: '100%',
+            justifyContent: 'flex-start',
+            pl: 2.5,
+            py: 1
+          }}
+        >
+          Add Column
+        </Button>
+      </Box>
     </Box>
   )
 }
