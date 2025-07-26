280d7d360efa8feffede15ed1bc8dbe53a8de9d4
DucSang404 <ndsang404@gmail.com>
2025-07-26 21:18:29 +0700

    connect to mongodb

--- api/src/config/mongodb.js
+++ api/src/config/mongodb.js
@@ -1,1 +1,33 @@
-//
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from '~/config/environment'
+
+let trelloDatabaseInstance = null
+
+// Object use for connect to Mongodb
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+// Connect to database
+export const CONNECT_DB = async () => {
+  console.log(process.env.MONGODB_URI)
+  await mongoClientInstance.connect()
+
+  // Get database name
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+}
+
+export const CLOSE_DB = async () => {
+  console.log('close db')
+  await mongoClientInstance.close()
+}
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Database first!')
+  return trelloDatabaseInstance
+}
+
