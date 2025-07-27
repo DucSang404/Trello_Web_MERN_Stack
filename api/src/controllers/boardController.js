69765ccba7a9d8b5077e681d8f9890a35d617ae9
DucSang404 <ndsang404@gmail.com>
2025-07-27 21:37:56 +0700

    error handlling by middleware

--- api/src/controllers/boardController.js
+++ api/src/controllers/boardController.js
@@ -2,16 +2,12 @@ import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (req, res, next) => {
   try {
-    console.log('req.body:', req.body)
-    console.log('req.query', req.query)
+    // console.log('req.body:', req.body)
+    // console.log('req.query', req.query)
 
+    // throw new ApiError(StatusCodes.BAD_GATEWAY, 'test')
     res.status(StatusCodes.CREATED).json({ message: 'POST CONTROLLER' })
-  } catch (error) {
-    console.log(error)
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
-  }
+  } catch (error) { next(error) }
 }
 
 export const boardController = {
--- api/src/validations/boardValidation.js
+++ api/src/validations/boardValidation.js
@@ -1,5 +1,6 @@
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
@@ -18,10 +19,9 @@ const createNew = async (req, res, next) => {
     // next step after middleware
     next()
   } catch (error) {
-    console.log(error)
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message
-    })
+    const errorMessage = new Error(error).message
+    const customError = new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, errorMessage)
+    next(customError)
   }
 }
 
